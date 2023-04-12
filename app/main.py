eeb660ae66c779df761cb4b460cb12c5d3ee2b92
Batkhuu Byambajav <batkhuu10@gmail.com>
2023-04-12 23:44:23 +0900

    :construction_worker: Updated CI/CD.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 from typing import Union
 
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, Request
+from dotenv import load_dotenv
+
+from __version__ import __version__
+
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -16,3 +24,42 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/api/v1/ping")
+async def ping(request: Request):
+
+    _response = {
+        "message": "Pong!",
+        "links": {
+            "self": "/api/v1/ping"
+        },
+        "meta": {
+            "api_version": "v1",
+            "version": __version__,
+            "method": request.method
+        }
+    }
+    return _response
+
+
+@app.get("/api/v1/health")
+async def health(request: Request):
+
+    _response = {
+        "message": "Everything is OK.",
+        "data": {},
+        "links": {
+            "self": "/api/v1/health"
+        },
+        "meta": {
+            "api_version": "v1",
+            "version": __version__,
+            "method": request.method
+        }
+    }
+    return _response
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("FASTAPI_PORT", "8000")), access_log=False)
