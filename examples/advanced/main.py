d2f8cd7de3fee631b20b89ec111796386d589264
Batkhuu Byambajav <batkhuu10@gmail.com>
2024-12-11 14:34:11 +0900

    :technologist: Add asynchronous example with aiohttp and update requirements

---
+++ examples/advanced/main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+## Standard libraries
+import sys
+import logging
+import pprint
+import asyncio
+from typing import Dict, Any
+
+## Third-party libraries
+import aiohttp
+
+
+logger = logging.getLogger(__name__)
+
+
+async def main() -> None:
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+    _base_url = "http://localhost:8000"
+    _api_prefix = "/api/v1"
+    _endpoint = "/ping"
+    _method = "GET"
+
+    _url = f"{_base_url}{_api_prefix}{_endpoint}"
+    _payload = {}
+    _headers = {"Accept": "application/json"}
+
+    logger.info("Sending request...")
+    _result_dict: Dict[str, Any] = {}
+    async with aiohttp.ClientSession() as _http_session:
+        async with _http_session.request(
+            method=_method, url=_url, headers=_headers, json=_payload
+        ) as _response:
+            _result_dict = await _response.json()
+    logger.info("Done!\n")
+
+    logging.info(f"\n{pprint.pformat(_result_dict)}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
